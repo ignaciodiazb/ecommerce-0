9fb3c5ea69801cbdfecdb1f764b038dc43194dc8
ignaciodb <ignaciodiazbarraza@gmail.com>
2021-02-01 00:21:21 -0300

    added view, url, and fetch call to send the databa bout the product that is being added, but for now it just return a simple response, a csrf token was also added

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -39,3 +40,7 @@ def checkout(request):
     context = {'items': items, 'order': order}
 
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('This item was added', safe=False)
