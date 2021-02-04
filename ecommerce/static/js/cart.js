ab74ac8de99f8d065917b09c30d9188beae34a82
ignaciodb <ignaciodiazbarraza@gmail.com>
2021-02-04 19:23:41 -0300

    added functionality to add and remove items from the cart and store them in the cookies

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -8,11 +8,37 @@ for (var i = 0; i < updateBtns.length; i++){
 
         console.log('User: ', user)
         if(user === 'AnonymousUser'){
-            console.log('Not logged in');
+            addCookieItem(productID, action);
         }else {
             updateUserOrder(productID, action);
         }
     });
+}
+
+function addCookieItem(productID, action) {
+    console.log('Not logged in..');
+
+    if(action == 'add') {
+        if(cart[productID] == undefined) {
+            cart[productID] = {'quantity':1};
+        }else {
+            cart[productID]['quantity'] += 1;
+        }
+    }
+
+    if(action == 'remove') {
+        cart[productID]['quantity'] -= 1;
+
+        if (cart[productID]['quantity'] <= 0) {
+            console.log('remove item');
+            delete cart[productID];
+        }
+    }
+
+    console.log('Cart: ', cart);
+    document.cookie = 'cart=' + JSON.stringify(cart) + ';domain=;path=/';
+    location.reload();
+
 }
 
 function updateUserOrder(productID, action) {
